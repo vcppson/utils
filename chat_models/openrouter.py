b7821551d65583bb182b0863478852a5a5457f11
vcppson <112033577+vcppson@users.noreply.github.com>
2025-08-05 18:44:36 +0900

    Add files via upload

---
+++ chat_models/openrouter.py
@@ -0,0 +1,23 @@
+import os
+from typing import Optional
+
+from dotenv import load_dotenv
+from langchain_core.utils.utils import secret_from_env
+from langchain_openai import ChatOpenAI
+from pydantic import Field, SecretStr
+
+load_dotenv()
+
+class ChatOpenRouter(ChatOpenAI):
+    openai_api_key: Optional[SecretStr] = Field(
+        alias="api_key", default_factory=secret_from_env("OPENROUTER_API_KEY", default=None)
+    )
+    @property
+    def lc_secrets(self) -> dict[str, str]:
+        return {"openai_api_key": "OPENROUTER_API_KEY"}
+
+    def __init__(self,
+                 openai_api_key: Optional[str] = None,
+                 **kwargs):
+        openai_api_key = openai_api_key or os.environ.get("OPENROUTER_API_KEY")
+        super().__init__(base_url="https://openrouter.ai/api/v1", openai_api_key=openai_api_key, **kwargs)
